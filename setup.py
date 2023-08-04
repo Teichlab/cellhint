945483566d8ac88541ea9670563ea89cb79e94df
ChuanXu1 <cx1@sanger.ac.uk>
2023-08-04 11:30:55 +0100

    Add requirements

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
-        "Development Status :: 5 - Production/Stable",
+        "Development Status :: 4 - Beta",
     ],
     python_requires='>=3.6',
 )
